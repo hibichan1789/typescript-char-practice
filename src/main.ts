db327da8a183053bcc9ece82770f05f670b23925
hibiki <takechi.hibiki1234@gmail.com>
2026-02-17 23:29:04 +0900

    散布図の練習

--- src/main.ts
+++ src/main.ts
@@ -1,107 +1,73 @@
 import { Chart } from "chart.js/auto";
 import type { ChartConfiguration } from "chart.js/auto";
-interface IceCreamSales{
-  month:number;
-  sales:number;
+
+interface StudyData {
+  hours: number;
+  score: number;
 }
-interface IceCreamSalesInfo {
-  year:number;
-  monthSales:IceCreamSales[];
+
+const studyResults: StudyData[] = [
+  { hours: 2, score: 40 },
+  { hours: 5, score: 55 },
+  { hours: 10, score: 65 },
+  { hours: 15, score: 85 },
+  { hours: 20, score: 95 }
+];
+
+const canvas = document.querySelector<HTMLCanvasElement>("#canvas");
+function init(){
+  if(!canvas){
+    throw new Error();
+  }
+  renderChart(canvas, studyResults, "学習時間とスコアの相関");
 }
-// モックデータ
-const iceCreamSalesInfo1:IceCreamSalesInfo = {
-  year:2026,
-  monthSales:[
-  {month:1, sales:120},
-  {month:2, sales:150},
-  {month:3, sales:200},
-  {month:4, sales:180},
-  {month:5, sales:250},
-  {month:6, sales:300}
-  ]
-}
-const iceCreamSalesInfo2:IceCreamSalesInfo = {
-  year:2025,
-  monthSales:[
-  {month:1, sales:100},
-  {month:2, sales:130},
-  {month:3, sales:210},
-  {month:4, sales:190},
-  {month:5, sales:240},
-  {month:6, sales:280}
-  ]
-}
-const chartTitle = "2026年上半期,2025年上半期売上推移比較"
-const salesCanvas = document.querySelector<HTMLCanvasElement>("#sales-canvas");
-function getMonths(iceCreamSalesInfo:IceCreamSalesInfo):string[]{
-  return iceCreamSalesInfo.monthSales.map(sales => `${sales.month}月`);
-}
-function getSales(iceCreamSalesInfo:IceCreamSalesInfo):number[]{
-  return iceCreamSalesInfo.monthSales.map(sales => sales.sales);
-}
-function setIceCreamChartConfig(iceCreamSalesInfo1:IceCreamSalesInfo,iceCreamSalesInfo2:IceCreamSalesInfo, chartTitle:string):ChartConfiguration<"bar"|"line">{
-  const months = getMonths(iceCreamSalesInfo1);
-  const sales1 = getSales(iceCreamSalesInfo1);
-  const sales2 = getSales(iceCreamSalesInfo2);
-  const iceCreamChartConfig:ChartConfiguration<"bar"|"line"> = {
-    type:"bar",
+
+
+function setChartConfig(data:StudyData[], chartTitle:string):ChartConfiguration<"scatter">{
+  const chartConfig:ChartConfiguration<"scatter"> = {
+    type:"scatter",
     data:{
-      labels:months,
       datasets:[{
-        label:`${iceCreamSalesInfo1.year}年`,
-        data:sales1,
-        backgroundColor:"#a0d8ef",
-        order:2
-      },
-      {
-        type:"line",
-        label:`${iceCreamSalesInfo2.year}年`,
-        data:sales2,
-        backgroundColor: "#ee1133",
-        order:1
-      }
-    ]
+        data:data.map(d => ({x:d.hours, y:d.score}))
+      }]
     },
     options:{
       scales:{
+        x:{
+          min:0,
+          title:{
+            display:true,
+            text:"勉強時間(h)"
+          }
+        },
         y:{
-          min:0
+          min:0,
+          title:{
+            display:true,
+            text:"テスト正解率(%)"
+          }
         }
       },
       plugins:{
         title:{
-          text:chartTitle,
           display:true,
-          font:{
-            size:20
-          }
+          text:chartTitle
         },
         legend:{
-          display:true
-        },
-        tooltip:{
-          callbacks:{
-            label:(context)=>{
-              return `${context.dataset.label}: ${context.formattedValue}個`
-            }
-          }
+          display:false
         }
       }
     }
   }
-  return iceCreamChartConfig;
+  return chartConfig;
 }
-function renderChart(canvas:HTMLCanvasElement, iceCreamSalesInfo1:IceCreamSalesInfo, iceCreamSalesInfo2:IceCreamSalesInfo, chartTitle:string):void{
+function renderChart(canvas:HTMLCanvasElement, data:StudyData[], chartTitle:string):void{
   const renderedChart = Chart.getChart(canvas);
   if(renderedChart){
     renderedChart.destroy();
   }
-  const iceCreamChartConfig = setIceCreamChartConfig(iceCreamSalesInfo1, iceCreamSalesInfo2, chartTitle);
-  new Chart(canvas, iceCreamChartConfig);
+  const chartConfig = setChartConfig(data, chartTitle);
+  new Chart(canvas, chartConfig);
 }
-function init(){
-  if(salesCanvas){
-    renderChart(salesCanvas, iceCreamSalesInfo1, iceCreamSalesInfo2, chartTitle);
-  }
-}
+
 window.addEventListener("DOMContentLoaded", init);
